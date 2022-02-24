28d49306b209fb1107d55ea89a35c7a88bb3986e
Tom Gregory <t.k.gregory@gmail.com>
2022-02-24 11:34:42 +0000

    Add Java toolchain config.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,3 +8,9 @@ dependencies {
     implementation(libs.spring.boot.web)
     testImplementation(libs.spring.boot.test)
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
